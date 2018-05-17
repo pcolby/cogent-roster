87f3e33e967743c8769adcbf07cd2c1d3ef1200c
Paul Colby <git@colby.id.au>
2018-05-18 00:28:40 +1000

    Implement no-single-days-off constraint

--- src/NoSingleDaysOff.h
+++ src/NoSingleDaysOff.h
@@ -23,11 +23,40 @@ public:
      */
     int constrain(QStringSet &nurses, const QString &shift, const QVariantList &daysSoFar) override
     {
-        Q_UNUSED(nurses);
         Q_UNUSED(shift);
-        Q_UNUSED(daysSoFar);
-        return 0;
 
+        // If we don't have 1 day of history yet, then no need to exclude anyone.
+        if (daysSoFar.size() < 2) { // 2 because daysSoFar always include the current day too.
+            qDebug() << "removed 0 of" << nurses.size() << "nurses";
+            return 0;
+        }
+
+        // Find which nurses had yesterday off.
+        QStringSet nursesWithYesterdayOff = nurses;
+        foreach (const QVariant &shift, daysSoFar.at(daysSoFar.size()-2).toMap()) {
+            foreach (const QVariant &nurse, shift.toList()) {
+                nursesWithYesterdayOff.remove(nurse.toString());
+            }
+        }
+        qDebug() << nursesWithYesterdayOff.size() << "of" << nurses.size() << "nurses had yesterday off";
+
+        // Find which nurses had (at least) two days off.
+        QStringSet nursesWithLastTwoDaysOff = nursesWithYesterdayOff;
+        if (daysSoFar.size() > 2) { // If we had two (or more) days of history.
+            foreach (const QVariant &shift, daysSoFar.at(daysSoFar.size()-2).toMap()) {
+                foreach (const QVariant &nurse, shift.toList()) {
+                    nursesWithLastTwoDaysOff.remove(nurse.toString());
+                }
+            }
+        }
+        qDebug() << nursesWithLastTwoDaysOff.size() << "of" << nurses.size() << "nurses had two days off";
+
+        // Remove all nurses with yesterday off, but not two (or more) days off yet (they need to
+        // have today off to allow for their days off to be grouped in two or more days).
+        const int originalSize = nurses.size();
+        nurses.subtract(nursesWithYesterdayOff - nursesWithLastTwoDaysOff);
+        qDebug() << "removed" << (originalSize-nurses.size()) << "of" << originalSize << "nurses";
+        return originalSize-nurses.size();
     }
 
 };
